efec57ed8d291651e91e21f503f634614bf5e890
mtang34705 <mtang34705@yahoo.com>
2021-07-15 17:58:54 +0100

    Getting User Data
    Added api to get user data

---
+++ TRMDataManager/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TRMDataManager.Library.DataAccess;
+using TRMDataManager.Library.Internal.Models;
+
+namespace TRMDataManager.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/User")]
+    public class UserController : System.Web.Http.ApiController
+    {
+        // GET: User/Details/5
+        public List<UserModel> GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+            UserData data = new UserData();
+
+            return data.GetUserById(userId);
+        }
+
+       
+    }
+}
